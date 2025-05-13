308cde28cef4a9e7e67e0d611ef4876acf70e47d
dmsqja <bum17822@naver.com>
2025-05-13 13:02:57 +0900

    feat: 애플 워치 앱 관련 파일 추가 및 설정
    - 애플 워치 앱의 기본 구조 및 주요 컴포넌트 추가
    - ContentView 및 HaptiTalkWatchApp 파일 생성
    - 앱 아이콘 및 색상 설정을 위한 리소스 추가
    - 테스트 파일 및 UI 테스트 설정 추가
    - .gitignore 파일에 Xcode 관련 항목 추가

---
+++ frontend/watch/HaptiTalkWatch/HaptiTalkWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HaptiTalkWatch Watch App
+//
+//  Created by 이은범 on 5/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
